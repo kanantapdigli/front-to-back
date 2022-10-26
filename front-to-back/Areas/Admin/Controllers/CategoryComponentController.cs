71a3d39d060adc437f2f9541ff696c80c04fa6e8
KananTapdigli <kanan.tapdigli@gmail.com>
2022-10-26 20:54:15 +0400

    one to many relation crud modified

--- front-to-back/Areas/Admin/Controllers/CategoryComponentController.cs
+++ front-to-back/Areas/Admin/Controllers/CategoryComponentController.cs
@@ -99,5 +99,85 @@ namespace front_to_back.Areas.Admin.Controllers
 
             return RedirectToAction("index");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var categoryComponent = await _appDbContext.CategoryComponents.FindAsync(id);
+            if (categoryComponent == null) return NotFound();
+
+            var model = new CategoryComponentUpdateViewModel
+            {
+                Title = categoryComponent.Title,
+                Description = categoryComponent.Description,
+                CategoryId = categoryComponent.CategoryId,
+                Categories = await _appDbContext.Categories.Select(c => new SelectListItem
+                {
+                    Text = c.Title,
+                    Value = c.Id.ToString()
+                })
+                .ToListAsync(),
+                PhotoPath = categoryComponent.FilePath
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, CategoryComponentUpdateViewModel model)
+        {
+            model.Categories = await _appDbContext.Categories.Select(c => new SelectListItem
+            {
+                Text = c.Title,
+                Value = c.Id.ToString()
+            })
+            .ToListAsync();
+
+            if (!ModelState.IsValid) return View(model);
+
+            if (id != model.Id) return BadRequest();
+
+            var categoryComponent = await _appDbContext.CategoryComponents.FindAsync(model.Id);
+            if (categoryComponent == null) return NotFound();
+
+            bool isExist = await _appDbContext.CategoryComponents
+                            .AnyAsync(cc => cc.Title.ToLower().Trim() == model.Title.ToLower().Trim()
+                                            && cc.Id != categoryComponent.Id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Title", "Bu adda kateqoriya komponent mövcuddur");
+                return View(model);
+            }
+
+            categoryComponent.Title = model.Title;
+            categoryComponent.Description = model.Description;
+
+            if (model.Photo != null)
+            {
+                if (!_fileService.IsImage(model.Photo))
+                {
+                    ModelState.AddModelError("Photo", "Yüklənən fayl şəkil formatında olmalıdır");
+                    return View(model);
+                }
+
+                if (!_fileService.CheckSize(model.Photo, 300))
+                {
+                    ModelState.AddModelError("Photo", "Şəklin ölçüsü 300 kb-dan aşağı olmalıdır");
+                    return View(model);
+                }
+
+                _fileService.Delete(_webHostEnvironment.WebRootPath, categoryComponent.FilePath);
+                categoryComponent.FilePath = await _fileService.UploadAsync(model.Photo, _webHostEnvironment.WebRootPath);
+            }
+
+            var category = await _appDbContext.Categories.FindAsync(model.CategoryId);
+            if (category == null) return NotFound();
+
+            categoryComponent.CategoryId = category.Id;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction("index");
+        }
     }
 }
